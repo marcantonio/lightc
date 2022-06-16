b6c8fc98913a06e288dcd9764f23ea54e6c49052
Marc Soda <marc@soda.fm>
2022-06-15 21:21:59 -0400

    major update to light-mode

--- misc/light-mode.el
+++ misc/light-mode.el
@@ -1,3 +1,11 @@
+;; Major mode for the Light programming language
+;; Resources:
+;;   - https://www.emacswiki.org/emacs/ModeTutorial
+;;   - https://www.omarpolo.com/post/writing-a-major-mode.html
+;;   - https://www.gnu.org/software/emacs/manual/html_node/elisp/index.html
+;;   - https://github.com/dominikh/go-mode.el
+;;   - https://github.com/rust-lang/rust-mode
+
 (defvar light-mode-hook nil)
 
 (defun light-runner ()
@@ -19,46 +27,87 @@
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.lt\\'" . light-mode))
 
-;(regexp-opt '("extern" "else" "if" "for" "let" "fn"))
+(defconst light-keywords
+  '("extern"
+    "else"
+    "if"
+    "for"
+    "let"
+    "fn"
+    "true"
+    "false"))
+
+(defconst light-builtin-type
+  '("int" "uint"
+    "int8" "uint8"
+    "int16" "uint16"
+    "int32" "uint32"
+    "int64" "uint64"
+    "float" "double"
+    "bool" "char"))
+
+;; Matches fn `foo'()
+(defconst light-fn-name-re "\\(?:fn\\|extern\\)[[:space:]]+\\([[:word:]]+\\)")
+
+;; Matches fn foo() -> `int'
+(defconst light-fn-return-re "\\(?:fn\\|extern\\)[[:space:]]+[[:word:]]+.+->[[:space:]]+\\([[:word:]]+\\)")
+
+;; Matches a `int' or [`int; 3']
+(defconst light-type-re "\\[?\\([[:word:]]+\\)\\(?:[[:space:]]*;[[:space:]]+[[:digit:]]+\\]\\)?")
+
+;; Matches `foo': int
+(defconst light-typed-decl-re (concat "\\([[:word:]]+\\)[[:space:]]*:[[:space:]]+" light-type-re))
+
+;; Identifies a closer. Used to skip further indentation when a block is closing
+(defconst light-closing-brace-re "^[[:space:]]*[})].*$")
+
 (defconst light-font-lock-keywords
-  (list
-   '("\\<\\(?:e\\(?:lse\\|xtern\\)\\|f\\(?:n\\|or\\)\\|if\\|let\\)\\>" . font-lock-keyword-face)
-   '("//.*" . font-lock-comment-face)
-   '("\".*\"" . font-lock-string-face)))
+  (append
+   `(
+     (,light-fn-name-re . (1 'font-lock-function-name-face))
+     (,light-fn-return-re . (1 'font-lock-type-face))
+     (,light-typed-decl-re . ((1 'font-lock-variable-name-face)
+                              (2 'font-lock-type-face)))
+     (,(regexp-opt light-builtin-type 'symbols) . font-lock-type-face)
+     (,(regexp-opt light-keywords 'symbols) . font-lock-keyword-face))))
+
+;; Helper to pull out the current index level
+(defun light-indent-level ()
+  (car (syntax-ppss)))
+
+;; Find the indent level of the last opening paren/brace/etc. It works by jumping to the
+;; beginning of the line (this lets us ignore other parens on the line), jumping back to
+;; the last opener, then returning the current indent level
+(defun light-find-enclosing-ident ()
+  (let ((searching t))
+    (save-excursion
+      (beginning-of-line)
+      (let ((opener-pos (nth 1 (syntax-ppss))))
+        (when opener-pos
+          (goto-char opener-pos)))
+      (light-indent-level))))
 
 (defun light-indent-line ()
-  "Indent current line as Light code"
+  "Indent current line"
   (interactive)
-  (let ((pos (- (point-max) (point))))
+  (let ((pos (- (point-max) (point))) ; used to follow the text if it moves
+        (enclosing-indent (light-find-enclosing-ident)) ; ident of the last seen opener
+        (ident 0))
     (beginning-of-line)
-    (if (bobp)
-        (indent-line-to 0)
-      (let ((not-indented t) indent)
-        (if (or (looking-at "^[ \t]*}[ \t]*\\(?://\\)?.*$")
-                (looking-at "^[ \t]*}[ \t]*else[ \t]*{[ \t]*\\(?://\\)?.*$"))
-            (progn
-              (save-excursion
-                (forward-line -1)
-                (setq indent (- (current-indentation) tab-width)))
-              (if (< indent 0)
-                  (setq indent 0)))
-          (save-excursion
-            (while not-indented
-              (forward-line -1)
-              (cond ((looking-at ".*{[ \t]*\\(?://\\)?.*$")
-                     (progn
-                       (setq indent (+ (current-indentation) tab-width))
-                       (setq not-indented nil)))
-                    ((looking-at "^[ \t]*}")
-                     (progn
-                       (setq indent (current-indentation))
-                       (setq not-indented nil)))
-                    ((bobp)
-                     (setq not-indented nil))))))
-        (when indent
-          (indent-line-to indent)
-          (if (> (- (point-max) pos) (point))
-	      (goto-char (- (point-max) pos))))))))
+    ;; If the enclosing indent and the current indent match, we are at the top level and
+    ;; shouldn't increase. This prevents openers from indenting
+    (if (or (= enclosing-indent (light-indent-level))
+            (looking-at light-closing-brace-re))
+        (setq indent (* enclosing-indent tab-width))
+      (setq indent (+ (* enclosing-indent tab-width) tab-width)))
+
+    (indent-line-to indent)
+
+    ;; If the cursor was in the indent space before any chars, do nothing (will jump to the
+    ;; first char because of `indent-line-to`). Otherwise, jump to maintain the current
+    ;; distance (pos) from the end of the buffer
+    (if (> (- (point-max) pos) (point))
+        (goto-char (- (point-max) pos)))))
 
 (defvar light-mode-syntax-table
   (let ((st (make-syntax-table)))
@@ -66,16 +115,20 @@
     (modify-syntax-entry ?/ ". 124b" st)
     (modify-syntax-entry ?* ". 23" st)
     (modify-syntax-entry ?\n "> b" st)
+    (modify-syntax-entry ?\" "\"" st)
+    (modify-syntax-entry ?\' "\"" st)
     st)
   "Syntax table for light-mode")
 
 (defvar light-mode-tab-width 4 "Tab width in Light mode")
 
 (define-derived-mode light-mode prog-mode "Light"
-  "Major mode for Light code"
+  "Major mode for the Light programming language"
   :syntax-table light-mode-syntax-table
   (set (make-local-variable 'font-lock-defaults) '(light-font-lock-keywords))
   (set (make-local-variable 'indent-line-function) 'light-indent-line)
+  (set (make-local-variable 'comment-start) "// ")
+  (set (make-local-variable 'comment-start-skip) "//[[:space:]]*")
   (when light-mode-tab-width
     (setq tab-width light-mode-tab-width)))
 
